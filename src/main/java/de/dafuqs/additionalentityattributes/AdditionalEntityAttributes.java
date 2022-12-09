f7e683d8fb635e240f6f0374b7193bc8e20db6b9
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-12-09 13:11:34 +0100

    3 new attributes; add aea to all mixin definitions; formatting; bumped version

--- src/main/java/de/dafuqs/additionalentityattributes/AdditionalEntityAttributes.java
+++ src/main/java/de/dafuqs/additionalentityattributes/AdditionalEntityAttributes.java
@@ -3,9 +3,8 @@ package de.dafuqs.additionalentityattributes;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.entity.attribute.ClampedEntityAttribute;
 import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class AdditionalEntityAttributes implements ModInitializer {
 	
@@ -56,19 +55,40 @@ public class AdditionalEntityAttributes implements ModInitializer {
 	 */
 	public static final EntityAttribute LAVA_VISIBILITY = createAttribute("lava_visibility", 1.0, 0, 1024.0);
 
-	/* 
-		Controls the dig speed of the player
+	/**
+	 * Controls the dig speed of the player
 	*/
 	public static final EntityAttribute DIG_SPEED = createAttribute("generic.dig_speed", 0.0D, 0.0D, 2048.0D);
 	
+	/**
+	 * Controls the drops the player gets when using enchantments, such as looting or fortune
+	 * (more precise: everything that uses the ApplyBonusLootFunction to increase drops based on an enchantments level)
+	 * Each full +1 on this stat will roll the bonus count another time. Highest one is kept.
+	 */
+	public static final EntityAttribute BONUS_LOOT_COUNT_ROLLS = createAttribute("generic.bonus_loot_count_rolls", 0.0D, 0.0D, 128.0);
+
+	/**
+	 *
+	 */
+	public static final EntityAttribute BONUS_RARE_LOOT_ROLLS = createAttribute("generic.bonus_rare_loot_rolls", 0.0D, 0.0D, 128.0);
+	
+	/**
+	 * Modifies the experience dropped from mining blocks and killing mobs.
+	 * The default of 1.0 equals the vanilla drop amount, 0.0 will result in no xp drops altogether
+	 */
+	public static final EntityAttribute DROPPED_EXPERIENCE = createAttribute("player.dropped_experience", 1.0D, 0.0D, 1024.0D);
+	
 	@Override
 	public void onInitialize() {
-		Registry.register(Registries.ATTRIBUTE, new Identifier(MOD_ID, "critical_bonus_damage"), CRITICAL_BONUS_DAMAGE);
-		Registry.register(Registries.ATTRIBUTE, new Identifier(MOD_ID, "water_speed"), WATER_SPEED);
-		Registry.register(Registries.ATTRIBUTE, new Identifier(MOD_ID, "water_visibility"), WATER_VISIBILITY);
-		Registry.register(Registries.ATTRIBUTE, new Identifier(MOD_ID, "lava_speed"), LAVA_SPEED);
-		Registry.register(Registries.ATTRIBUTE, new Identifier(MOD_ID, "lava_visibility"), LAVA_VISIBILITY);
-        Registry.register(Registries.ATTRIBUTE, new Identifier(MOD_ID, "dig_speed"), DIG_SPEED);
+		Registry.register(Registry.ATTRIBUTE, new Identifier(MOD_ID, "critical_bonus_damage"), CRITICAL_BONUS_DAMAGE);
+		Registry.register(Registry.ATTRIBUTE, new Identifier(MOD_ID, "water_speed"), WATER_SPEED);
+		Registry.register(Registry.ATTRIBUTE, new Identifier(MOD_ID, "water_visibility"), WATER_VISIBILITY);
+		Registry.register(Registry.ATTRIBUTE, new Identifier(MOD_ID, "lava_speed"), LAVA_SPEED);
+		Registry.register(Registry.ATTRIBUTE, new Identifier(MOD_ID, "lava_visibility"), LAVA_VISIBILITY);
+        Registry.register(Registry.ATTRIBUTE, new Identifier(MOD_ID, "dig_speed"), DIG_SPEED);
+		Registry.register(Registry.ATTRIBUTE, new Identifier(MOD_ID, "bonus_rare_loot_rolls"), BONUS_RARE_LOOT_ROLLS);
+		Registry.register(Registry.ATTRIBUTE, new Identifier(MOD_ID, "bonus_loot_count_rolls"), BONUS_LOOT_COUNT_ROLLS);
+        Registry.register(Registry.ATTRIBUTE, new Identifier(MOD_ID, "dropped_experience"), DROPPED_EXPERIENCE);
 	}
 	
 	private static EntityAttribute createAttribute(final String name, double base, double min, double max) {
--- src/main/java/de/dafuqs/additionalentityattributes/mixin/common/PlayerEntityMixin.java
+++ src/main/java/de/dafuqs/additionalentityattributes/mixin/common/PlayerEntityMixin.java
@@ -2,47 +2,51 @@ package de.dafuqs.additionalentityattributes.mixin.common;
 
 import de.dafuqs.additionalentityattributes.AdditionalEntityAttributes;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
-public class PlayerEntityMixin {
+public abstract class PlayerEntityMixin {
+	
+	@Inject(method = "createPlayerAttributes()Lnet/minecraft/entity/attribute/DefaultAttributeContainer$Builder;", require = 1, allow = 1, at = @At("RETURN"))
+	private static void additionalEntityAttributes$addPlayerAttributes(final CallbackInfoReturnable<DefaultAttributeContainer.Builder> info) {
+		info.getReturnValue().add(AdditionalEntityAttributes.DROPPED_EXPERIENCE);
+	}
 	
 	/**
 	 * By default, the additional crit damage is a 50% bonus
 	 */
 	@ModifyConstant(method = "attack(Lnet/minecraft/entity/Entity;)V", constant = @Constant(floatValue = 1.5F))
-	public float applyCriticalDamageMultiplierAttribute(float original) {
+	public float additionalEntityAttributes$applyCriticalBonusDamage(float original) {
 		EntityAttributeInstance criticalDamageMultiplier = ((LivingEntity) (Object) this).getAttributeInstance(AdditionalEntityAttributes.CRITICAL_BONUS_DAMAGE);
-		if(criticalDamageMultiplier == null) {
+		if (criticalDamageMultiplier == null) {
 			return original;
 		} else {
 			return 1 + (float) criticalDamageMultiplier.getValue();
 		}
 	}
-
-    @ModifyVariable(method = "getBlockBreakingSpeed", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/effect/StatusEffectUtil;hasHaste(Lnet/minecraft/entity/LivingEntity;)Z"), index = 2)
-	private float getBlockBreakingSpeedMixin(float f) {
+	
+	@ModifyVariable(method = "getBlockBreakingSpeed", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/effect/StatusEffectUtil;hasHaste(Lnet/minecraft/entity/LivingEntity;)Z"), index = 2)
+	private float additionalEntityAttributes$adjustBlockBreakingSpeed(float f) {
 		EntityAttributeInstance instance = ((LivingEntity) (Object) this).getAttributeInstance(AdditionalEntityAttributes.DIG_SPEED);
-
+		
 		if (instance != null) {
 			for (EntityAttributeModifier modifier : instance.getModifiers()) {
 				float amount = (float) modifier.getValue();
-
+				
 				if (modifier.getOperation() == EntityAttributeModifier.Operation.ADDITION)
 					f += amount;
 				else
 					f *= (amount + 1);
 			}
 		}
-
+		
 		return f;
 	}
-
+	
 }
