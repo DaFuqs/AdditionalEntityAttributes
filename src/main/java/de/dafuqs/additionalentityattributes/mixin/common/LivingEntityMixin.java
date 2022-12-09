f61164aee44f7c8cb83b879ffb94597c871e1302
Noaaan <noaaan@hotmail.com>
2022-12-09 14:15:13 +0100

    Add Magic Protection attribute

--- src/main/java/de/dafuqs/additionalentityattributes/mixin/common/LivingEntityMixin.java
+++ src/main/java/de/dafuqs/additionalentityattributes/mixin/common/LivingEntityMixin.java
@@ -7,6 +7,7 @@ import net.fabricmc.api.Environment;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.tag.FluidTags;
@@ -19,87 +20,102 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-	
-	@Shadow
-	@Nullable
-	protected PlayerEntity attackingPlayer;
-	
-	@Inject(method = "createLivingAttributes()Lnet/minecraft/entity/attribute/DefaultAttributeContainer$Builder;", require = 1, allow = 1, at = @At("RETURN"))
-	private static void additionalEntityAttributes$addAttributes(final CallbackInfoReturnable<DefaultAttributeContainer.Builder> info) {
-		info.getReturnValue().add(AdditionalEntityAttributes.WATER_VISIBILITY);
-		info.getReturnValue().add(AdditionalEntityAttributes.WATER_SPEED);
-		info.getReturnValue().add(AdditionalEntityAttributes.LAVA_VISIBILITY);
-		info.getReturnValue().add(AdditionalEntityAttributes.LAVA_SPEED);
-		info.getReturnValue().add(AdditionalEntityAttributes.CRITICAL_BONUS_DAMAGE);
-		info.getReturnValue().add(AdditionalEntityAttributes.DIG_SPEED);
-		info.getReturnValue().add(AdditionalEntityAttributes.BONUS_LOOT_COUNT_ROLLS);
-		info.getReturnValue().add(AdditionalEntityAttributes.BONUS_RARE_LOOT_ROLLS);
-	}
-	
-	@ModifyArg(method = "travel(Lnet/minecraft/util/math/Vec3d;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;updateVelocity(FLnet/minecraft/util/math/Vec3d;)V", ordinal = 0))
-	public float additionalEntityAttributes$waterSpeed(float original) {
-		EntityAttributeInstance waterSpeed = ((LivingEntity) (Object) this).getAttributeInstance(AdditionalEntityAttributes.WATER_SPEED);
-		if (waterSpeed == null) {
-			return original;
-		} else {
-			if (waterSpeed.getBaseValue() != original) {
-				waterSpeed.setBaseValue(original);
-			}
-			return (float) waterSpeed.getValue();
-		}
-	}
-	
-	@ModifyConstant(method = "swimUpward", constant = @Constant(doubleValue = 0.03999999910593033D))
-	public double additionalEntityAttributes$modifyUpwardSwimming(double original, TagKey<Fluid> fluid) {
-		if (fluid == FluidTags.WATER) {
-			EntityAttributeInstance waterSpeed = ((LivingEntity) (Object) this).getAttributeInstance(AdditionalEntityAttributes.WATER_SPEED);
-			if (waterSpeed == null) {
-				return original;
-			} else {
-				if (waterSpeed.getBaseValue() != original) {
-					waterSpeed.setBaseValue(original);
-				}
-				return waterSpeed.getValue();
-			}
-		} else {
-			return original;
-		}
-	}
-	
-	@Environment(EnvType.CLIENT)
-	@ModifyConstant(method = "knockDownwards", constant = @Constant(doubleValue = -0.03999999910593033D))
-	public double additionalEntityAttributes$knockDownwards(double original) {
-		EntityAttributeInstance waterSpeed = ((LivingEntity) (Object) this).getAttributeInstance(AdditionalEntityAttributes.WATER_SPEED);
-		if (waterSpeed == null) {
-			return original;
-		} else {
-			if (waterSpeed.getBaseValue() != -original) {
-				waterSpeed.setBaseValue(-original);
-			}
-			return -waterSpeed.getValue();
-		}
-	}
-	
-	@ModifyConstant(method = "travel", constant = {@Constant(doubleValue = 0.5D, ordinal = 0), @Constant(doubleValue = 0.5D, ordinal = 1), @Constant(doubleValue = 0.5D, ordinal = 2)})
-	private double additionalEntityAttributes$increasedLavaSpeed(double original) {
-		EntityAttributeInstance lavaSpeed = ((LivingEntity) (Object) this).getAttributeInstance(AdditionalEntityAttributes.LAVA_SPEED);
-		if (lavaSpeed == null) {
-			return original;
-		} else {
-			if (lavaSpeed.getBaseValue() != original) {
-				lavaSpeed.setBaseValue(original);
-			}
-			return lavaSpeed.getValue();
-		}
-	}
-	
-	@ModifyArg(method = "dropXp()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ExperienceOrbEntity;spawn(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/Vec3d;I)V"), index = 2)
-	protected int additionalEntityAttributes$modifyExperience(int originalXP) {
-		if (this.attackingPlayer == null) {
-			return originalXP;
-		} else {
-			return (int) (originalXP * Support.getExperienceMod(this.attackingPlayer));
-		}
-	}
-	
+
+    @Shadow
+    @Nullable
+    protected PlayerEntity attackingPlayer;
+
+    @Inject(method = "createLivingAttributes()Lnet/minecraft/entity/attribute/DefaultAttributeContainer$Builder;", require = 1, allow = 1, at = @At("RETURN"))
+    private static void additionalEntityAttributes$addAttributes(final CallbackInfoReturnable<DefaultAttributeContainer.Builder> info) {
+        info.getReturnValue().add(AdditionalEntityAttributes.WATER_VISIBILITY);
+        info.getReturnValue().add(AdditionalEntityAttributes.WATER_SPEED);
+        info.getReturnValue().add(AdditionalEntityAttributes.LAVA_VISIBILITY);
+        info.getReturnValue().add(AdditionalEntityAttributes.LAVA_SPEED);
+        info.getReturnValue().add(AdditionalEntityAttributes.CRITICAL_BONUS_DAMAGE);
+        info.getReturnValue().add(AdditionalEntityAttributes.DIG_SPEED);
+        info.getReturnValue().add(AdditionalEntityAttributes.BONUS_LOOT_COUNT_ROLLS);
+        info.getReturnValue().add(AdditionalEntityAttributes.BONUS_RARE_LOOT_ROLLS);
+        info.getReturnValue().add(AdditionalEntityAttributes.MAGIC_PROTECTION);
+    }
+
+    @ModifyArg(method = "travel(Lnet/minecraft/util/math/Vec3d;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;updateVelocity(FLnet/minecraft/util/math/Vec3d;)V", ordinal = 0))
+    public float additionalEntityAttributes$waterSpeed(float original) {
+        EntityAttributeInstance waterSpeed = ((LivingEntity) (Object) this).getAttributeInstance(AdditionalEntityAttributes.WATER_SPEED);
+        if (waterSpeed == null) {
+            return original;
+        } else {
+            if (waterSpeed.getBaseValue() != original) {
+                waterSpeed.setBaseValue(original);
+            }
+            return (float) waterSpeed.getValue();
+        }
+    }
+
+    @ModifyConstant(method = "swimUpward", constant = @Constant(doubleValue = 0.03999999910593033D))
+    public double additionalEntityAttributes$modifyUpwardSwimming(double original, TagKey<Fluid> fluid) {
+        if (fluid == FluidTags.WATER) {
+            EntityAttributeInstance waterSpeed = ((LivingEntity) (Object) this).getAttributeInstance(AdditionalEntityAttributes.WATER_SPEED);
+            if (waterSpeed == null) {
+                return original;
+            } else {
+                if (waterSpeed.getBaseValue() != original) {
+                    waterSpeed.setBaseValue(original);
+                }
+                return waterSpeed.getValue();
+            }
+        } else {
+            return original;
+        }
+    }
+
+    @Environment(EnvType.CLIENT)
+    @ModifyConstant(method = "knockDownwards", constant = @Constant(doubleValue = -0.03999999910593033D))
+    public double additionalEntityAttributes$knockDownwards(double original) {
+        EntityAttributeInstance waterSpeed = ((LivingEntity) (Object) this).getAttributeInstance(AdditionalEntityAttributes.WATER_SPEED);
+        if (waterSpeed == null) {
+            return original;
+        } else {
+            if (waterSpeed.getBaseValue() != -original) {
+                waterSpeed.setBaseValue(-original);
+            }
+            return -waterSpeed.getValue();
+        }
+    }
+
+    @ModifyConstant(method = "travel", constant = {@Constant(doubleValue = 0.5D, ordinal = 0), @Constant(doubleValue = 0.5D, ordinal = 1), @Constant(doubleValue = 0.5D, ordinal = 2)})
+    private double additionalEntityAttributes$increasedLavaSpeed(double original) {
+        EntityAttributeInstance lavaSpeed = ((LivingEntity) (Object) this).getAttributeInstance(AdditionalEntityAttributes.LAVA_SPEED);
+        if (lavaSpeed == null) {
+            return original;
+        } else {
+            if (lavaSpeed.getBaseValue() != original) {
+                lavaSpeed.setBaseValue(original);
+            }
+            return lavaSpeed.getValue();
+        }
+    }
+
+    @ModifyArg(method = "dropXp()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ExperienceOrbEntity;spawn(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/Vec3d;I)V"), index = 2)
+    protected int additionalEntityAttributes$modifyExperience(int originalXP) {
+        if (this.attackingPlayer == null) {
+            return originalXP;
+        } else {
+            return (int) (originalXP * Support.getExperienceMod(this.attackingPlayer));
+        }
+    }
+
+    @ModifyVariable(method = "applyArmorToDamage", at = @At("HEAD"), argsOnly = true)
+    private float additionalEntityAttributes$reduceMagicDamage(float damage, DamageSource source) {
+        EntityAttributeInstance magicProt = ((LivingEntity) (Object) this).getAttributeInstance(AdditionalEntityAttributes.MAGIC_PROTECTION);
+
+        if (magicProt == null) {
+            return damage;
+        }
+
+        if (source.isMagic() && magicProt.getValue() > 0) {
+            damage = (float) Math.max(damage - magicProt.getValue(), 0);
+        }
+        return damage;
+    }
+
 }
