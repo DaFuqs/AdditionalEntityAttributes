123ec7da1174ddc3a4ce9bb567e7c1179c9825ed
Noaaan <noaaan@hotmail.com>
2023-04-09 18:32:22 +0200

    Fix a Mixin refmap issue, and a handful of smaller things, bumps version
    Signed-off-by: Noaaan <noaaan@hotmail.com>

--- src/main/java/de/dafuqs/additionalentityattributes/mixin/common/BlockMixin.java
+++ src/main/java/de/dafuqs/additionalentityattributes/mixin/common/BlockMixin.java
@@ -17,17 +17,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = Block.class)
 public abstract class BlockMixin {
-	
+
 	private PlayerEntity additionalEntityAttributes$breakingPlayer;
-	
-	@ModifyArg(method = "dropExperience", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ExperienceOrbEntity;spawn(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/Vec3d;I)V"), index = 2)
+
+	@ModifyArg(method = "dropExperience", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ExperienceOrbEntity;spawn(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/Vec3d;I)V"))
 	private int additionalEntityAttributes$modifyExperience(int originalXP) {
 		return (int) (originalXP * Support.getExperienceMod(additionalEntityAttributes$breakingPlayer));
 	}
-	
+
 	@Inject(method = "afterBreak", at = @At("HEAD"))
 	public void additionalEntityAttributes$saveBreakingPlayer(World world, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack, CallbackInfo callbackInfo) {
 		additionalEntityAttributes$breakingPlayer = player;
 	}
-	
+
 }
