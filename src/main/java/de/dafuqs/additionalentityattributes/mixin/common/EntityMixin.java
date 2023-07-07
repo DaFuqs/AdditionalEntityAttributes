1e965fb0bebcd04bf39d424e2da9291facc5b7f5
Noaaan <noaaan@hotmail.com>
2023-07-07 18:40:00 +0200

    Fix default air value, allow negative max air, use MixinExtras for better compatibility
    Signed-off-by: Noaaan <noaaan@hotmail.com>

--- src/main/java/de/dafuqs/additionalentityattributes/mixin/common/EntityMixin.java
+++ src/main/java/de/dafuqs/additionalentityattributes/mixin/common/EntityMixin.java
@@ -1,17 +1,17 @@
 package de.dafuqs.additionalentityattributes.mixin.common;
 
+import com.llamalad7.mixinextras.injector.*;
 import de.dafuqs.additionalentityattributes.*;
 import net.minecraft.entity.*;
 import net.minecraft.entity.attribute.*;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.*;
 
 @Mixin(Entity.class)
-public class EntityMixin {
-    @Inject(method = "getMaxAir", at = @At("HEAD"), cancellable = true)
-    public void getMaxAir(CallbackInfoReturnable<Integer> cir) {
-        int original = 300;
+public abstract class EntityMixin {
+
+    @ModifyReturnValue(method = "getMaxAir", at = @At("RETURN"))
+    public int getMaxAir(int original) {
         if (!((Object) this instanceof LivingEntity livingEntity)) {
             cir.setReturnValue(original);
         } else {
