1f6cbc9078994ed5571add57f20d8788e11debad
DaFuqs <dominik.m.schmidt@googlemail.com>
2025-08-24 10:20:54 +0200

    fix memory leak by saving player reference weak. Fixes #44

--- common/src/main/java/de/dafuqs/additionalentityattributes/mixin/common/BlockMixin.java
+++ common/src/main/java/de/dafuqs/additionalentityattributes/mixin/common/BlockMixin.java
@@ -16,20 +16,25 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.lang.ref.*;
+
 @Mixin(Block.class)
 public abstract class BlockMixin {
 	
 	@Unique
-	private Player additionalEntityAttributes$breakingPlayer;
+	private WeakReference<Player> additionalEntityAttributes$breakingPlayer;
 
 	@Inject(method = "playerDestroy", at = @At("HEAD"))
 	public void additionalEntityAttributes$saveBreakingPlayer(Level level, Player player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack, CallbackInfo callbackInfo) {
-		additionalEntityAttributes$breakingPlayer = player;
+		additionalEntityAttributes$breakingPlayer = new WeakReference<>(player);
 	}
 	
 	@ModifyArg(method = "popExperience", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/ExperienceOrb;award(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/phys/Vec3;I)V"))
 	private int additionalEntityAttributes$modifyExperience(int originalXP) {
-		return (int) (originalXP * Support.getExperienceMod(additionalEntityAttributes$breakingPlayer));
+		if(additionalEntityAttributes$breakingPlayer.get() != null) {
+			return (int) (originalXP * Support.getExperienceMod(additionalEntityAttributes$breakingPlayer.get()));
+		}
+		return originalXP;
 	}
 
 
