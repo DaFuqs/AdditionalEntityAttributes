cff73bdf2013d36c4b7c63171f60ef7a0e89a028
Naz <bluspringindohq@gmail.com>
2025-12-15 07:29:49 +0800

    Fixed more environment mixins

--- common/src/main/java/de/dafuqs/additionalentityattributes/mixin/client/LavaFogEnvironmentMixin.java
+++ common/src/main/java/de/dafuqs/additionalentityattributes/mixin/client/LavaFogEnvironmentMixin.java
@@ -3,6 +3,7 @@ package de.dafuqs.additionalentityattributes.mixin.client;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import de.dafuqs.additionalentityattributes.AdditionalEntityAttributes;
+import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.fog.environment.LavaFogEnvironment;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -13,8 +14,8 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(LavaFogEnvironment.class)
 public abstract class LavaFogEnvironmentMixin {
     @ModifyExpressionValue(method = "setupFog", at = @At(value = "CONSTANT", args = "floatValue=0.25F", ordinal = 0))
-    private static float additionalEntityAttributes$modifyLavaVisibilityMinWithoutFireResistance(float original, @Local(argsOnly = true) Entity entity) {
-        AttributeInstance lavaVisibilityAttribute = entity instanceof LivingEntity living ? living.getAttribute(AdditionalEntityAttributes.LAVA_VISIBILITY) : null;
+    private static float additionalEntityAttributes$modifyLavaVisibilityMinWithoutFireResistance(float original, @Local(argsOnly = true) Camera camera) {
+        AttributeInstance lavaVisibilityAttribute = camera.entity() instanceof LivingEntity living ? living.getAttribute(AdditionalEntityAttributes.LAVA_VISIBILITY) : null;
         if (lavaVisibilityAttribute == null) {
             return original;
         } else {
@@ -26,8 +27,8 @@ public abstract class LavaFogEnvironmentMixin {
     }
 
     @ModifyExpressionValue(method = "setupFog", at = @At(value = "CONSTANT", args = "floatValue=1.0F", ordinal = 0))
-    private static float additionalEntityAttributes$modifyLavaVisibilityMaxWithoutFireResistance(float original, @Local(argsOnly = true) Entity entity) {
-        AttributeInstance lavaVisibilityAttribute = entity instanceof LivingEntity living ? living.getAttribute(AdditionalEntityAttributes.LAVA_VISIBILITY) : null;
+    private static float additionalEntityAttributes$modifyLavaVisibilityMaxWithoutFireResistance(float original, @Local(argsOnly = true) Camera camera) {
+        AttributeInstance lavaVisibilityAttribute = camera.entity() instanceof LivingEntity living ? living.getAttribute(AdditionalEntityAttributes.LAVA_VISIBILITY) : null;
         if (lavaVisibilityAttribute == null) {
             return original;
         } else {
@@ -39,8 +40,8 @@ public abstract class LavaFogEnvironmentMixin {
     }
 
     @ModifyExpressionValue(method = "setupFog", at = @At(value = "CONSTANT", args = "floatValue=0.0F", ordinal = 0))
-    private static float additionalEntityAttributes$modifyLavaVisibilityMinFireResistance(float original, @Local(argsOnly = true) Entity entity) {
-        AttributeInstance lavaVisibilityAttribute = entity instanceof LivingEntity living ? living.getAttribute(AdditionalEntityAttributes.LAVA_VISIBILITY) : null;
+    private static float additionalEntityAttributes$modifyLavaVisibilityMinFireResistance(float original, @Local(argsOnly = true) Camera camera) {
+        AttributeInstance lavaVisibilityAttribute = camera.entity() instanceof LivingEntity living ? living.getAttribute(AdditionalEntityAttributes.LAVA_VISIBILITY) : null;
         if (lavaVisibilityAttribute == null) {
             return original;
         } else {
@@ -52,8 +53,8 @@ public abstract class LavaFogEnvironmentMixin {
     }
 
     @ModifyExpressionValue(method = "setupFog", at = @At(value = "CONSTANT", args = "floatValue=5.0F", ordinal = 0))
-    private static float additionalEntityAttributes$modifyLavaVisibilityMaxWithFireResistance(float original, @Local(argsOnly = true) Entity entity) {
-        AttributeInstance lavaVisibilityAttribute = entity instanceof LivingEntity living ? living.getAttribute(AdditionalEntityAttributes.LAVA_VISIBILITY) : null;
+    private static float additionalEntityAttributes$modifyLavaVisibilityMaxWithFireResistance(float original, @Local(argsOnly = true) Camera camera) {
+        AttributeInstance lavaVisibilityAttribute = camera.entity() instanceof LivingEntity living ? living.getAttribute(AdditionalEntityAttributes.LAVA_VISIBILITY) : null;
         if (lavaVisibilityAttribute == null) {
             return original;
         } else {
