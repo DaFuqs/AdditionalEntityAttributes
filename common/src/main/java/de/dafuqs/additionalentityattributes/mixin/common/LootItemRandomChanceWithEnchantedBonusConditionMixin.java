7c40666bf5de747e8c6dc0d66ff39762220c9078
DaFuqs <dominik.m.schmidt@googlemail.com>
2026-03-16 22:02:45 +0100

    handle lookups with enchantment levels == 0; fixes https://github.com/TeamAOF/Craftoria/issues/795

--- common/src/main/java/de/dafuqs/additionalentityattributes/mixin/common/LootItemRandomChanceWithEnchantedBonusConditionMixin.java
+++ common/src/main/java/de/dafuqs/additionalentityattributes/mixin/common/LootItemRandomChanceWithEnchantedBonusConditionMixin.java
@@ -15,8 +15,6 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LootItemRandomChanceWithEnchantedBonusCondition.class)
 public abstract class LootItemRandomChanceWithEnchantedBonusConditionMixin {
@@ -36,7 +34,9 @@ public abstract class LootItemRandomChanceWithEnchantedBonusConditionMixin {
 			AttributeInstance attributeInstance = livingEntity.getAttribute(AdditionalEntityAttributes.BONUS_RARE_LOOT_ROLLS);
 			if (attributeInstance != null) {
 				int level = EnchantmentHelper.getEnchantmentLevel(this.enchantment, livingEntity);
-				return lootContext.getRandom().nextFloat() < this.enchantedChance.calculate(level) * (float) attributeInstance.getValue();
+				if(level > 0) {
+					return lootContext.getRandom().nextFloat() < this.enchantedChance.calculate(level) * (float) attributeInstance.getValue();
+				}
 			}
 		}
 		return original;
